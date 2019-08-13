1bfd00c4fed583ee0fe7b39d99aca2e79f007494
tinevancorenland <tinevancorenland@gmail.com>
2019-08-13 16:19:11 +0200

    delete redundant code that was commented out for note overview

--- listnotes.php
+++ listnotes.php
@@ -4,7 +4,6 @@ require "connection.php";
 
 $overview = "SELECT * FROM notes";
 $overviewresult = $conn->query($overview);
-$result = array();
 
 if($overviewresult->num_rows > 0) {
     while($row = $overviewresult->fetch_assoc()) {
@@ -14,18 +13,6 @@ if($overviewresult->num_rows > 0) {
 } else {
     echo "no results";
 }
-
-// $overview = "SELECT * FROM notes";
-// $result = mysql_query($conn, $overview);
-// $rows = [];
-
-// if(mysql_num_rows($result) > 0) {
-//     while($row = mysqli_fetch_assoc($result)) {
-//         array_push($rows, $row);
-//     }
-
-//     print json_encode($rows);
-// }
 
 mysqli_close($conn);
 
