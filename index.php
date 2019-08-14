a1b80cdf436d231bb8564ce80ca229ff7173a4a0
tinevancorenland <tinevancorenland@gmail.com>
2019-08-14 14:59:54 +0200

    echo the result of the new input in an html table;

--- index.php
+++ index.php
@@ -9,22 +9,38 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php require_once "process.php" ?>
+    <?php 
+    require_once "process.php"
+    ?>
+
+    <?php 
+    
+    $conn = mysqli_connect($servername, $username, $password, $database) or die(mysqli_error($conn));
+    
+    $result = $conn->query("SELECT * FROM notes") or die($conn->error);
+
+    ?>
+
     <div class="notes">
         <h1 class="header">Notes</h1>
         <table id="notetable">
-            <tr>
-            <th id="headtitle">title</th>
-            <th id="headcontent">content</th>
-            </tr>
-            <tr>
-                <td class="title">title one</td>
-                <td class="content">content one</td>
-            </tr>
-            <tr>
-                <td class="title">title two</td>
-                <td class="content">content two</td>
-            </tr>
+            <thead>
+                <tr>
+                <th id="headtitle">title</th>
+                <th id="headcontent">content</th>
+                <th id="headaction">action</th>
+                </tr>
+            </thead>
+
+                <?php 
+                while ($row = $result->fetch_assoc()): ?>
+                <tr>
+                    <td class="title"><?php echo $row["title"]; ?></td>
+                    <td class="content"><?php echo $row["content"]; ?></td>
+                    <td></td>
+                </tr>
+                <?php endwhile; ?>
+
         </table>
         <!-- <ul class="items">
             <li class="item">
@@ -38,7 +54,7 @@
         </ul> -->
         <form class="item-add" action="process.php" method="post">
             <input class="title-add" type="text" name="title" placeholder="title" autocomplete="off" required>
-            <input class="content-add" type="text" name="content" size="1000" placeholder="content" ></textarea>
+            <input class="content-add" type="text" name="content"  size="1000" placeholder="content" ></textarea>
             <input class="submit" type="submit" name="save" value="add new note" >
         </form>
 
